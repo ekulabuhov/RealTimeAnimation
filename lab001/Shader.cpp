be9c2d29860026b606e95c10554d4891b46646f9
Dave Ganter <ganterd@tcd.ie>
2015-10-15 12:09:09 +0100

    Loading, compiling and linking shaders

--- lab001/Shader.cpp
+++ lab001/Shader.cpp
@@ -1,14 +1,62 @@
 #include "Shader.hpp"
 
+Shader::Shader()
+{
+	this->_fragShaderID = NULL;
+	this->_vertShaderID = NULL;
+	this->_shaderProgramID = NULL;
+}
+
+void Shader::enableShader()
+{
+	glUseProgram(this->_shaderProgramID);
+}
+
+void Shader::disableShader()
+{
+	glUseProgram(NULL);
+}
+
 bool Shader::loadShadersFromFiles(std::string vertShaderPath, std::string fragShaderFile)
 {
+	/* Read the shaders from file */
 	std::string vertShaderSource = this->_readShaderFile(vertShaderPath);
 	std::string fragShaderSource = this->_readShaderFile(fragShaderFile);
 
+	/* Make sure we have shader sources */
 	if (vertShaderSource == "" || fragShaderSource == "")
 	{
 		return false;
 	}
+
+	/* Get GL to allocate shader space */
+	this->_vertShaderID = glCreateShader(GL_VERTEX_SHADER);
+	this->_fragShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	/* Compile the shaders */
+	bool vertShaderCompiled = this->_compileShader(vertShaderSource, GL_VERTEX_SHADER, this->_vertShaderID);
+	bool fragShaderCompiled = this->_compileShader(fragShaderSource, GL_FRAGMENT_SHADER, this->_fragShaderID);
+
+	/* Make sure shaders compiled */
+	if (!vertShaderCompiled || !fragShaderCompiled)
+	{
+		return false;
+	}
+
+	/* Get GL to allocate shader program space */
+	this->_shaderProgramID = glCreateProgram();
+
+	/* Attach vert and frag shaders to shader program */
+	bool linked = this->_linkShaders(this->_shaderProgramID, this->_vertShaderID, this->_fragShaderID);
+
+	if (!linked)
+	{
+		return false;
+	}
+
+	/* Always detach after successful link. See: https://www.opengl.org/wiki/Shader_Compilation */
+	glDetachShader(this->_shaderProgramID, this->_vertShaderID);
+	glDetachShader(this->_shaderProgramID, this->_fragShaderID);
 
 	return true;
 }
@@ -27,4 +75,57 @@ std::string Shader::_readShaderFile(std::string shaderPath)
 	std::string shaderSource = buffer.str();
 
 	return shaderSource;
+}
+
+bool Shader::_compileShader(std::string shaderSource, GLuint shaderType, GLuint shaderID)
+{
+	const char* vShaderSourceChar = shaderSource.c_str();
+	const GLint vShaderSourceLength = shaderSource.length();
+	glShaderSource(shaderID, 1, &vShaderSourceChar, &vShaderSourceLength);
+
+	glCompileShader(shaderID);
+
+	GLint compiled;
+	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &compiled);
+	if (compiled == GL_FALSE)
+	{
+		GLint logLength = 0;
+		glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &logLength);
+		GLchar* errorString = new GLchar[logLength];
+		glGetShaderInfoLog(shaderID, logLength, &logLength, errorString);
+
+		const char* sType = shaderType == GL_VERTEX_SHADER ? "vertex" : "fragment";
+		std::cerr << "ERROR: Couldn't compile " << sType << " shader:" << std::endl << errorString << std::endl;
+		glDeleteShader(shaderID);
+		delete[] errorString;
+		return false;
+	}
+
+	return true;
+}
+
+bool Shader::_linkShaders(GLuint shaderProgram, GLuint vertShader, GLuint fragShader)
+{
+	glAttachShader(shaderProgram, vertShader);
+	glAttachShader(shaderProgram, fragShader);
+
+	glLinkProgram(shaderProgram);
+
+	GLint compiled;
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &compiled);
+	if (compiled == GL_FALSE)
+	{
+		GLint logLength = 0;
+		glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &logLength);
+		GLchar* errorString = new GLchar[logLength];
+		glGetProgramInfoLog(shaderProgram, logLength, &logLength, errorString);
+
+		std::cerr << "ERROR: Couldn't link shader program:" << std::endl << errorString << std::endl;
+		glDeleteProgram(shaderProgram);
+		glDeleteShader(vertShader);
+		glDeleteShader(fragShader);
+
+		delete[] errorString;
+		return false;
+	}
 }
--- lab001/Shader.hpp
+++ lab001/Shader.hpp
@@ -5,14 +5,26 @@
 #include <fstream>
 #include <sstream>
 
+#include <GL\glew.h>
+
 class Shader
 {
 public:
+	Shader();
+
 	void enableShader();
+	void disableShader();
+
 	bool loadShadersFromFiles(std::string vertShaderPath, std::string fragShaderPath);
 
 protected:
 	std::string _readShaderFile(std::string path);
+	bool _compileShader(std::string source, GLuint shaderType, GLuint shaderID);
+	bool _linkShaders(GLuint shaderProgram, GLuint vertShader, GLuint fragShader);
+
+	GLuint _vertShaderID;
+	GLuint _fragShaderID;
+	GLuint _shaderProgramID;
 };
 
 #endif
